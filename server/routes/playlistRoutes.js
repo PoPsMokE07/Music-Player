380735204de854b2b76dc63025a9be6115e6e576
Suryangsu Chandra <95937843+PoPsMokE07@users.noreply.github.com>
2024-08-30 00:43:11 +0530

    Update playlistRoutes.js

--- server/routes/playlistRoutes.js
+++ server/routes/playlistRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
 	createPlaylist,
-	editPlaylist,
 	getPlaylist,
 	getPlaylists,
 } from "../controllers/playlistController.js";
@@ -10,8 +9,8 @@ import { verifyToken } from "../middleware/validateToken.js";
 const router = express.Router();
 
 router.get("/", getPlaylists);
-router.get("/:id", getPlaylist);
 router.post("/create", verifyToken, createPlaylist);
-router.patch("/:id", verifyToken, editPlaylist);
+router.get("/:id", getPlaylist);
+
 
 export { router as playlistRouter };
